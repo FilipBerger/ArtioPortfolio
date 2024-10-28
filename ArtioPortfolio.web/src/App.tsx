baebc2591c1fd81228afa7e985821b6e9868a658
Filip Berger <filip-berger@hotmail.com>
2024-10-28 17:14:37 +0100

    Implemented initial header component.

--- ArtioPortfolio.web/src/App.tsx
+++ ArtioPortfolio.web/src/App.tsx
@@ -1,11 +1,14 @@
 import Gallery from "./components/Gallery/Gallery.tsx"
+import userData from "./mocks/mockUserData.json"
+import { User } from "./interfaces.tsx"
 
 function App() {
+  const typedUserData: User = userData as User;
 
   return (
-    <>
-      <Gallery />
-    </>
+    <div className="app--container">
+      <Gallery userData={typedUserData}/>
+    </div>
   )
 }
 
