9992d64a60e4b408fffa58a24be53f4f058f6904
Filip Berger <filip-berger@hotmail.com>
2024-10-30 09:29:05 +0100

    Removed unused import.

--- ArtioPortfolio.web/src/components/Gallery/Gallery.tsx
+++ ArtioPortfolio.web/src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import Header from "../Header/Header.tsx"
 import { GalleryProps } from "../../interfaces.tsx"
 import Miniature from "../Miniature/Miniature.tsx"
